6b98a3abaf615b8d39413568db4df45db213438e
Shivam khantwal <shivamkhantwal9410@gmail.com>
2023-11-03 07:22:54 +0530

    Added  checked icon

--- src/Benefit.js
+++ src/Benefit.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Benefits'
+import  checked from '../src/assets/checked.png'
 const Benefit = ({title,about}) => {
   return (
     <div class='d-flex ' style={{alignItems:'center'}}>
    
    <div className='svgicon'>
-   <svg class='m-3' xmlns="http://www.w3.org/2000/svg" width={30} height={30} viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>
-   
+<img src={checked} alt='checked'/>
    </div>
     <p> {title} </p>
     </div>
